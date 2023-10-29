539b07ce83834e3c1af107d7e6d51cb068e9530f
miloszwasacz <wasacz.dev@gmail.com>
2023-10-29 11:06:34 +0000

    Reformat Action to the right formatting convention

--- Actions/Action.cs
+++ Actions/Action.cs
@@ -2,20 +2,22 @@
 
 namespace GameJam2023MarsYeah.Actions;
 
-public abstract class Action{
-	public string ActionText{ get; private set; }
+public abstract class Action
+{
+    public string ActionText { get; private set; }
 
-	protected Action(string filename){
-		ActionText = FileAccess.GetFileAsString(filename);
-	}
+    protected Action(string filename)
+    {
+        ActionText = FileAccess.GetFileAsString(filename);
+    }
 
-	public abstract void OnSelect(GameState state);
+    public abstract void OnSelect(GameState state);
 
-	public abstract int GetPopularityEffect(GameState state);
+    public abstract int GetPopularityEffect(GameState state);
 
-	public abstract int GetDamage(GameState state);
+    public abstract int GetDamage(GameState state);
 
-	public abstract bool CanBeUsed(GameState state);
+    public abstract bool CanBeUsed(GameState state);
 
-	public abstract float GetProbability(GameState state);
+    public abstract float GetProbability(GameState state);
 }
