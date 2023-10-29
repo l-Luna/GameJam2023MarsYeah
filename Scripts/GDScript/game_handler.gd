b016df6389dd72f5517364a81a76bb1aa9796a4c
Leo Gratwick <xm22456@bristol.ac.uk>
2023-10-29 16:34:10 +0000

    something

--- Scripts/GDScript/game_handler.gd
+++ Scripts/GDScript/game_handler.gd
@@ -4,12 +4,6 @@ extends Node2D
 @onready var camera = $Camera2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		pause_game()
