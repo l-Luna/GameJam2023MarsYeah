0023296cb16e5d53508981503a2d2bc6bb268647
l-Luna <izzeldeen03@gmail.com>
2023-10-29 10:57:31 +0000

    export game state stuff in editor

--- GameState.cs
+++ GameState.cs
@@ -1,20 +1,19 @@
-﻿using Godot;
+using Godot;
 
 namespace GameJam2023MarsYeah;
 
-public class GameState : Node{
+public partial class GameState : Node{
 
-	public int HumanHealth, MartianHealth, Opinion;
+	[Export]
+	public int HumanHealth = 100, MartianHealth = 100, Opinion = -100;
 
-	public bool IsHumanTurn;
+	[Export]
+	public bool IsHumanTurn = true;
 
 	public RandomNumberGenerator Rng;
-	
+
 	public override void _Ready(){
 		base._Ready();
-		HumanHealth = MartianHealth = 100;
-		Opinion = -100;
-		IsHumanTurn = true;
 		// TODO: expose seed through UI
 		Rng = new RandomNumberGenerator();
 		Rng.Randomize();
