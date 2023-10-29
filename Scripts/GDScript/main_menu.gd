0794bc6fa5f2630e6500cfdbb91c7e605e122075
Leo Gratwick <xm22456@bristol.ac.uk>
2023-10-29 20:18:39 +0000

    animation for main menu

--- Scripts/GDScript/main_menu.gd
+++ Scripts/GDScript/main_menu.gd
@@ -3,7 +3,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$AnimationPlayer.play("rocket_fly")
 
 
 func _on_play_button_button_down():
