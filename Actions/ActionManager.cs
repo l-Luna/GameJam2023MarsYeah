6ac8400a5015f5fd2a26ccfd0d1716517967c535
l-Luna <izzeldeen03@gmail.com>
2023-10-29 10:48:01 +0000

    `ActionManager` and RNG

---
+++ Actions/ActionManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GameJam2023MarsYeah.Actions;
+
+public static class ActionManager{
+
+	public static readonly List<Action> All = new List<Action>(){
+		new InsultOpponent()
+	};
+
+	public static List<Action> GetValidActions(GameState state, int amount){
+		// all the actions where CanTrue returns true
+		List<Action> valid = All.Where(x => x.CanUse(state)).ToList();
+		// if we don't have enough actions, then we have a problem
+		if(amount > valid.Count)
+			throw new Exception("we don't have that many actions!");
+		// TODO: not technically correct but "good enough"
+		return valid
+			.OrderBy(x => state.Rng.Randf() * x.Probability(state))
+			.Take(amount)
+			.ToList();
+	}
+}
--- GameState.cs
+++ GameState.cs
@@ -7,11 +7,16 @@ public class GameState : Node{
 	public int HumanHealth, MartianHealth, Opinion;
 
 	public bool IsHumanTurn;
+
+	public RandomNumberGenerator Rng;
 	
 	public override void _Ready(){
 		base._Ready();
 		HumanHealth = MartianHealth = 100;
 		Opinion = -100;
 		IsHumanTurn = true;
+		// TODO: expose seed through UI
+		Rng = new RandomNumberGenerator();
+		Rng.Randomize();
 	}
 }
