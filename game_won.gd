49a07705d4fcab94b7cdbdebb50e7e67f9f118c1
Leo Gratwick <xm22456@bristol.ac.uk>
2023-10-29 16:09:34 +0000

    added game won scence

---
+++ game_won.gd
@@ -0,0 +1,26 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func set_player_won(player):
+	$VBoxContainer/RichTextLabel.text = player + " Won"
+
+func _on_quit_button_pressed():
+	get_tree().quit()
+
+func _on_back_to_menu_pressed():
+	var main_menu = load("res://Scenes/main_menu.tscn").instantiate()
+	var playButton = main_menu.get_node("VBoxContainer/Buttons/PlayButton")
+	playButton.connect("pressed", on_play_button)
+	get_parent().add_child(main_menu)
+	hide()
+	
+	
+func on_play_button():
+	var new_game = load("res://Scenes/game_handler.tscn").instantiate()
+	get_parent().add_child(new_game)
+	queue_free()
+	
