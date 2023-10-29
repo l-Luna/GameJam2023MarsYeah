66d7797c251ce6ef8cc29c8f312fff557c723de0
miloszwasacz <wasacz.dev@gmail.com>
2023-10-29 17:09:03 +0000

    Fix unused variable

--- Scripts/GDScript/main_menu.gd
+++ Scripts/GDScript/main_menu.gd
@@ -6,11 +6,6 @@ func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func _on_play_button_button_down():
 	var sceneHandler = $"/root/Scene Handler"
 	sceneHandler.ToGame(self)
