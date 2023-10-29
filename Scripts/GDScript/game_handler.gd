0d867bba9a737ea26b235cff99494cb995dd39e9
Leo Gratwick <xm22456@bristol.ac.uk>
2023-10-29 12:11:02 +0000

    ui for players

--- Scripts/GDScript/game_handler.gd
+++ Scripts/GDScript/game_handler.gd
@@ -10,16 +10,14 @@ func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
 		pan_camera()
 		
-
-
+	if event.is_action_pressed("ui_cancel"):
+		get_tree().quit()
+		
 
 func pan_camera():
 	if camera.offset == Vector2.ZERO:
