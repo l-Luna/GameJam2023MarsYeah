7a224f69c2d0b4397e6d2db02aad69a7bbcddee0
Leo Gratwick <xm22456@bristol.ac.uk>
2023-10-30 10:23:11 +0000

    floating brain

--- Scripts/GDScript/main_menu.gd
+++ Scripts/GDScript/main_menu.gd
@@ -4,7 +4,7 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimationPlayer.play("rocket_fly")
-	#$AnimationPlayer.play("eelon_fly")
+	$AnimationPlayer2.play("eelon_float")
 
 
 func _on_play_button_button_down():
